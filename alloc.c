9a5986d779c2d6a7825c677633e6a9452fea1bde
MoncefDrew <moncef.mokrani@etu.univ-batna2.dz>
2026-06-09 16:46:27 +0100

    finished the mkalloc

--- alloc.c
+++ alloc.c
@@ -3,11 +3,58 @@
 
 extern heap *memspace;
 
+void *mkalloc(word words, header *hdr){
+    void *ret ,*bytesin;
+    word wordsin;
+
+    bytesin = ($v ( ($v hdr) - memspace));
+    wordsin =  (((word)bytesin)/4)+1;
+    if(words > (Maxwords - wordsin))
+    reterr(ErrNoMem);
+
+    hdr->w = wordsin;
+    hdr->alloced = true;
+    ret = ($v hdr)+4;
+
+    return ret;
+}
+
+
+void *alloc(int32 bytes){
+
+    void *mem;
+    header *hdr;
+    word words;
+
+    words = (!(bytes % 4)) ? bytes/4 : (bytes/4)+1;
+
+    mem = $v  memspace;
+    hdr = $h  mem;
+
+
+    (!(hdr->w)) ? ({
+        if (words > Maxwords){
+            reterr(ErrNoMem);
+        }
+
+        mem = mkalloc(words , hdr);
+        if (!mem)
+           return $v 0;
+        return mem;
+
+    })
+:
+    ({
+        (void)0;
+    });
+
+      return $v 0;
+}
 
 int main(int argc ,char *argv[]){
-    int8 *p;
-    p=memspace;
-    *p = 'X';
 
+    int8 *p;
+    p = alloc(7);
+    printf("address is : 0x%x \n",$i p);
     return 0;
 }
